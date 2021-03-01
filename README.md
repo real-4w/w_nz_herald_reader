b4c9ef011e4091b4b5392281336f4e4992afd56c
real-4w <real_4w@protonmail.com>
2021-03-02 09:19:07 +1300

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # w_nz_herald_reader
 Willem's NZ Herald Reader - premium articles
+
+https://towardsdatascience.com/how-to-scrape-any-website-with-python-and-beautiful-soup-bc84e95a3483
--- w_nz_herald_prem.py
+++ w_nz_herald_prem.py
@@ -12,7 +12,7 @@ URLS = {"https://www.nzherald.co.nz/business/online-ghosts-facebooks-millennial-
 #==============================================================================
 def GetMySoup (URL):
     """This function takes a URL and returns a parsed soup."""
-    if debug == True : print("+-" * 30, "\nStep 1: Scraping URL: ", URL)
+    if debug == True : print("+-" * 30, "\nStep 1: Getting URL: ", URL)
     s1 = requests.Session()
     header1 = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
     page1 = requests.get(URL, headers=header1)
@@ -21,13 +21,60 @@ def GetMySoup (URL):
         input("Enter to continue...")
     my_soup = BeautifulSoup(page1.content, 'lxml')
     if debug == True :
-        print(my_soup)
+    #    print(my_soup)
         input("Enter to continue...")
     return my_soup
 
+#==============================================================================
+# step S: Scrape the #2 table: get the TDs
+#==============================================================================
+def ScrapeMySoup(soup):
+    """This function scrapes my soup object and returns a list of strings."""
+    if debug == True : print("+-" * 30, "\nStep 2: Scraping the Soup.")
+    s_content = soup.find_all("title")
+    if debug == True : print(s_content)
+
+    s_text = soup.find_all(text=True)
+    #if debug == True : print(s_text)
+    #https://matix.io/extract-text-from-webpage-using-beautifulsoup-and-python/
+
+    if debug == True : print(set([t.parent.name for t in s_text]))
+
+    output = ''
+    blacklist = [
+        '[document]',
+        'script',
+        'header',
+        'html',
+        'meta',
+        'head', 
+        'input',
+        'script',
+        #'p',                                # WvdS
+        'head',
+        'button',
+        'div',
+        'a',
+        'h1',
+        'h2',
+        'h3',
+        'h4',
+        'time',
+        'small',
+        #'span',                            # WvdS
+        #'title',                           # WvdS
+        'figcaption',
+        # there may be more elements you don't want, such as "style", etc.
+        ]
+
+    for t in s_text:
+        if t.parent.name not in blacklist:
+            output += '{} '.format(t)
+    print(output)
 
 if __name__ == "__main__" :                                     # execute only if run as a script
     for target in URLS :
         if debug == True :
             print(f"{URLS}")
         nzh_soup = GetMySoup(target)
+        ScrapeMySoup(nzh_soup)
