8917c2da8bbbfc4a2662d542de951c4c0cc41430
real-4w <real_4w@protonmail.com>
2021-10-19 07:43:10 +1300

    HTML tweak

--- w_nz_herald_prem_html.py
+++ w_nz_herald_prem_html.py
@@ -14,13 +14,14 @@ if __name__ == "__main__" :                                                 # ex
            <h2>Premium news the cheap way.</h2>\n</body></html>")
 
     for target in URLS :
-        w_html.write(f"<h2>{target}.</h2>")
+        w_html.write(f'<h2>{target}.</h2><p style="font-family:Arial">')
         if debug == True : print(f"{len(URLS)} URLS:{URLS}")
         nzh_soup = w_scrape.GetMySoup(target)
         article= w_scrape.ScrapeMySoup(nzh_soup)
         if debug == True : print(article)
         news_list.append(article)    
         w_html.write(article)
+        w_html.write(f"</p>")
         if debug == True :print(f"Length news_list is now {len(news_list)}")
     w_html.close()
     
