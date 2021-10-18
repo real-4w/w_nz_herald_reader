e1eae6cbb3b30a2015894ba7a116fccf23f35313
real-4w <real_4w@protonmail.com>
2021-10-18 13:42:00 +1300

    Write into HTML

---
+++ TEST/test_html_write.py
@@ -0,0 +1,11 @@
+
+Func = open("nz_h_p.html","w")
+  
+# Adding input data to the HTML file
+Func.write("<html>\n<head>\n<title> \nOutput Data in an HTML file\n \
+           </title>\n</head> <body> <h1>Welcome to \
+           <font color = #00b300>NZ Herald Premium Reader</font></h1>\n \
+           <h2>Premium news the cheap way</h2>\n</body></html>")
+  
+# Saving the data into the HTML file
+Func.close()
